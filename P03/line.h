606ed227f23d6e0c95c4ebbb6b89bef577c108ea
Hoang Luu <hoang.luu8@mavs.uta.edu>
2019-09-05 00:18:34 -0500

    seperated into cpp and h files

---
+++ P03/line.h
@@ -0,0 +1,10 @@
+#include "line.cpp"
+#include<string>
+class Line{
+  public:
+	Line(double x1,double y1, double x2, double y2);
+	std::string to_string();
+	double length();
+  private:
+	double _x1,_x2,_y1,_y2;
+};
---
+++ P03/main.cpp
@@ -0,0 +1,7 @@
+
+
+int main() {
+    Line line{0, 0, 0, 0};
+    std::cout << line.to_string() << " is "
+              << line.length() << " long" << std::endl;
+}
