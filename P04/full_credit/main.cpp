94fa9b1e58d7c017f65e27c800800d133288a2f5
Hoang Luu <hoang.luu8@mavs.uta.edu>
2019-09-24 01:32:25 -0500

    ful credit

--- P04/full_credit/main.cpp
+++ P04/full_credit/main.cpp
@@ -7,6 +7,7 @@
 #include "train.h"
 
 int main(){
+int w=7;
 Train train;
 Locomotive locomotive;
 Coach coach;
@@ -16,9 +17,10 @@ do{
   std::cout<<"  Hoang Luu Express 1000969998  "<<std::endl;
   std::cout<<"================================"<<std::endl;
   std::cout<<"\n"<<train.to_art()<<std::endl;
-
-
-
+    std::cout<<std::setw(7)<<"Minutes"<<std::setw(w)<<"1"<<std::setw(w)<<"5"<<std::setw(w)<<"15"<<std::setw(w)<<"30"<<std::setw(w)<<"60"<<std::endl;
+for(int i=0;i<5*w+7;i++) std::cout<<"-"; std::cout<<std::endl;
+ std::cout<<std::setw(7)<<"m/s"<<std::setw(w)<<train.speed(1)<<std::setw(w)<< std::setprecision(3)<<train.speed(5)<<std::setw(w)<<train.speed(15)<<std::setw(w)<<train.speed(30)<<std::setw(w)<<train.speed(60)<<std::endl;
+ 
   std::cout<< "\n1 - Add a locomotive\n2 - Add an empty coach\n9 - Clear the train"<<std::endl;
   std::cout<<"0 - Exit\n\nCommand?\n" <<std::endl;
 
--- P04/full_credit/train.cpp
+++ P04/full_credit/train.cpp
@@ -8,12 +8,15 @@ void Train::add_coach(Coach& coach){_coaches.push_back(&coach);std::cout<<"coach
 double Train::speed(double minutes){
   int total_power=0;
   int total_weight=0;
-  for(int i=0;i<_locomotives.size();i++){
+  total_power=_locomotives.size()*13500;
+  total_weight=_locomotives.size()*80000+_coaches.size()*80000;
+  /*for(int i=0;i<_locomotives.size();i++){
     total_power+=_locomotives[i]->power();
     total_weight+=_locomotives[i]->weight();
   }
   for(int i=0;i<_coaches.size();i++)
     total_weight+=_coaches[i]->weight();
+  */
   return std::sqrt(2*total_power*minutes*60/total_weight);
 }
 std::string Train::to_art(){
